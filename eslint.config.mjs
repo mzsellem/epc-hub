a8ea51b6b13ebdb06c19293254d001c04d7b8845
mzsellem <marleysellem@gmail.com>
2025-11-15 14:21:51 -0700

    add axe accessibility linter and eslint-plugin-jsx-a11y

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  // Add the jsx-a11y plugin + its recommended rules.
+  {
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      ...jsxA11y.configs.recommended.rules,
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
