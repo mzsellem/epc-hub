d86869f5828bccbeb1d48628d20e7a9ebb4dc880
mzsellem <marleysellem@gmail.com>
2025-11-17 18:11:39 -0700

    add style and format to home page font

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
           <img src="/logo.png" alt="EPC Hub Logo" width={125} height={125} />
         </div>
         <div>
-          <img src="/logo-title.png" alt="EPC Hub Text Logo" width={300} height={75} />
+          <img src="/logo-title.png" alt="EPC Hub Text Logo" width={400} height={100} />
         </div>
-        <ul className="flex space-x-8 text-2xl home-nav">
-          <li><a href="/about" className="hover:underline underline-offset-4">About</a></li>
-          <li><a href="/books" className="hover:underline underline-offset-4">Books</a></li>
-          <li><a href="/coaching" className="hover:underline underline-offset-4">Coaching</a></li>
-          <li><a href="/contact" className="hover:underline underline-offset-4">Contact</a></li>
+        <ul className="flex space-x-8 text-xl home-nav font-light">
+          <li><a href="/about" className="hover:underline underline-offset-4 decoration-1">About</a></li>
+          <li><a href="/books" className="hover:underline underline-offset-4 decoration-1">Books</a></li>
+          <li><a href="/coaching" className="hover:underline underline-offset-4 decoration-1">Coaching</a></li>
+          <li><a href="/contact" className="hover:underline underline-offset-4 decoration-1">Contact</a></li>
         </ul>
       </main>
     </div>
