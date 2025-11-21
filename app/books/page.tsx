14b520a3f8d2e86fc8a536f03e55f5392717d459
mzsellem <marleysellem@gmail.com>
2025-11-20 19:44:30 -0700

    contact page design in progress, add mailto

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,10 +1,11 @@
 import Header from "../Components/header/page";
+import Footer from "../Components/footer/page";
 
 export default function Books() {
   return (
     <>
       <Header />
-        
+      <Footer />  
     </>
   );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,42 @@
+import Header from '../Components/header/page';
+import Footer from '../Components/footer/page';
+
 export default function Contact() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div>
-            <h1>Contact</h1>
-        </div>
-      </main>
-    </div>
+    <>
+      <Header />
+
+      <div className="min-h-screen flex justify-center px-6 py-24 font-sans">
+        <main className="max-w-2xl w-full text-center space-y-12">
+
+          {/* Title */}
+          <h1 className="text-4xl font-semibold header">
+            Your story deserves our attention.
+          </h1>
+
+          {/* Description */}
+          <p className="text-lg leading-relaxed paragraph">
+            For speaking engagements, workshops, or other inquiries,<br />
+            connect with us.
+          </p>
+
+          {/* Divider */}
+          <div className="flex justify-center">
+            <div className="w-20 h-1 bg-[#DDAD11] rounded-full"></div>
+          </div>
+
+          {/* Email */}
+          <a 
+          href="mailto:kenyada@executiveparentcompany.com"
+          className="text-xl font-medium hover:underline underline-offset-4"
+          >
+            kenyada@executiveparentcompany.com
+          </a>
+
+        </main>
+      </div>
+
+      <Footer />
+    </>
   );
 }
